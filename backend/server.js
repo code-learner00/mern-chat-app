1b115166694d40c52cdb4a0da5179a9b1e9bbed2
BHAVYA SAI SRI <124481659+code-learner00@users.noreply.github.com>
2026-02-18 08:25:29 +0530

    Refactor server.js for improved readability

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 connectDB();
 const app = express();
 
-app.use(express.json()); // to accept json data
+app.use(express.json());
 
 // app.get("/", (req, res) => {
 //   res.send("API Running!");
@@ -21,7 +21,6 @@ app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-// --------------------------deployment------------------------------
 
 const __dirname1 = path.resolve();
 
@@ -37,9 +36,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-// --------------------------deployment------------------------------
 
-// Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
 
@@ -54,7 +51,7 @@ const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
     origin: "http://localhost:3000",
-    // credentials: true,
+   
   },
 });
 
